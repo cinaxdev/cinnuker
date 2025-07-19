f4315d5c66224ef8472f11f58104d4100f4944a8
CinaxDEV <cinaxytoffical@gmail.com>
2025-07-19 15:54:24 +0300

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -2,13 +2,14 @@
 title Cinnuker Setup
 color 0a
 
-echo [INFO] Installing required Python packages...
+echo [INFO] Installing and upgrading required Python packages...
 py -m ensurepip
-py -m pip install --upgrade pip
-py -m pip install discord
+py -m pip install --upgrade pip setuptools
+py -m pip install --upgrade certifi
+py -m pip install --upgrade discord.py aiohttp certifi
 
 echo [INFO] Downloading project zip...
-powershell -Command "Invoke-WebRequest -Uri 'https://cinnuker-project-nug1aawee-cinaxs-projects-31cda09b.vercel.app/cinnuker.zip' -OutFile 'cinnuker.zip'"
+powershell -Command "Invoke-WebRequest -Uri 'https://cinnuker-project-fcuate30s-cinaxs-projects-31cda09b.vercel.app/cinnuker.zip' -OutFile 'cinnuker.zip'"
 
 if exist "cinnuker.zip" (
     powershell -Command "Expand-Archive -Force 'cinnuker.zip' '.'"
